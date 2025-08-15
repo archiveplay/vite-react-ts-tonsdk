2238ba0a07fec9069c5801886d263b9deee02ea9
Vladislav Nefedov <clay.zenx@gmail.com>
2025-08-15 19:43:04 +0300

    try basename

--- src/main.tsx
+++ src/main.tsx
@@ -16,11 +16,17 @@ const queryClient = new QueryClient({
 // mock manifest
 const manifestUrl = "https://archiveplay.github.io/vite-react-ts-tonsdk/tonconnect-manifest.json";
 
+const basePath = window.location.pathname.split("/")[1]
+  ? `/${window.location.pathname.split("/")[1]}`
+  : "";
+
+console.log('basePath', basePath)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <TonConnectUIProvider manifestUrl={manifestUrl}>
-        <BrowserRouter>
+        <BrowserRouter basename={basePath}>
           <App />
         </BrowserRouter>
       </TonConnectUIProvider>
