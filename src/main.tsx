d5b2fe58f578577e6f4e11d03acb10311a023075
Vladislav Nefedov <clay.zenx@gmail.com>
2025-08-15 16:25:39 +0300

    add react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from 'react'
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { createRoot } from 'react-dom/client'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+})
 
 // mock manifest
 const manifestUrl = "https://clayzenx.github.io/twa-gta/tonconnect-manifest.json";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <App />
-    </TonConnectUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <App />
+      </TonConnectUIProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
